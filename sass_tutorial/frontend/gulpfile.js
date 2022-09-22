26590469231a3c39eaf7f944e768f6bf5a7dbcc0
ekity1002 <dreamguidance1@gmail.com>
2022-09-22 09:04:39 +0900

    sass_tutorial 環境構築, tutorial2

---
+++ sass_tutorial/frontend/gulpfile.js
@@ -0,0 +1,20 @@
+// gulp用設定スクリプト
+const {src, dest, watch, series} = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+
+function buildStyles() {
+  // index.scssをもとにして、cssをビルドする
+  return src('index.scss')
+  // sassコンパイル実行
+    .pipe(sass())
+    // cssフォルダー以下に保存
+    .pipe(dest('css'))
+}
+
+function watchTask() {
+  // index.scss の変更をwatchして buildStyleを実行する関数
+  watch(['index.scss'], buildStyles)
+}
+
+// gulp コマンドで実行する関数の設定. build, watchが起動する
+exports.default = series(buildStyles, watchTask)
